00565926c7fa4243c2ad80d5f04ca3880299c972
NaingMinThant77 <naingminthant07@gmail.com>
2026-01-03 23:15:11 +0630

    fix: Stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,19 @@
-// import { Stack } from "expo-router";
-// import "../global.css";
-// import React from "react";
+import { Stack } from "expo-router";
+import "../global.css";
 
-// const Layout = () => {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index" options={{ headerShown: false }} />
-//       <Stack.Screen name="forecast" options={{ headerShown: false }} />
-//     </Stack>
-//   );
-// };
+import React from "react";
 
-// export default Layout;
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="forecast" options={{ headerShown: false }} />
+      <Stack.Screen name="qrcode" options={{ title: "React Native Course" }} />
+    </Stack>
+  );
+};
+
+export default Layout;
 
 // import { Slot } from "expo-router";
 
@@ -19,47 +21,48 @@
 
 // export default Slot;
 
-import { Slot } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect, useState, useCallback } from "react";
-import { View } from "react-native";
-import "../global.css";
+//Splash Screen
+// import { Slot } from "expo-router";
+// import * as SplashScreen from "expo-splash-screen";
+// import { useEffect, useState, useCallback } from "react";
+// import { View } from "react-native";
+// import "../global.css";
 
-// Prevent the splash screen from auto-hiding
-SplashScreen.preventAutoHideAsync();
+// // Prevent the splash screen from auto-hiding
+// SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const [appIsReady, setAppIsReady] = useState(false);
+// export default function RootLayout() {
+//   const [appIsReady, setAppIsReady] = useState(false);
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Simulate loading (e.g., waiting for your weather store to init)
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-    prepare();
-  }, []);
+//   useEffect(() => {
+//     async function prepare() {
+//       try {
+//         // Simulate loading (e.g., waiting for your weather store to init)
+//         await new Promise((resolve) => setTimeout(resolve, 2000));
+//       } catch (e) {
+//         console.warn(e);
+//       } finally {
+//         setAppIsReady(true);
+//       }
+//     }
+//     prepare();
+//   }, []);
 
-  // This function is called when the root view layout is complete
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
+//   // This function is called when the root view layout is complete
+//   const onLayoutRootView = useCallback(async () => {
+//     if (appIsReady) {
+//       // This tells the splash screen to hide
+//       await SplashScreen.hideAsync();
+//     }
+//   }, [appIsReady]);
 
-  if (!appIsReady) {
-    return null;
-  }
+//   if (!appIsReady) {
+//     return null;
+//   }
 
-  return (
-    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-      <Slot />
-    </View>
-  );
-}
+//   return (
+//     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+//       <Slot />
+//     </View>
+//   );
+// }
